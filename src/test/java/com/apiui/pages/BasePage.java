7a431224553ebecc748236159a6a0e6485f000a1
Abdullah13706 <127724839+Abdullah13706@users.noreply.github.com>
2023-08-11 18:53:40 +0300

    BasePage Created

--- src/test/java/com/apiui/pages/BasePage.java
+++ src/test/java/com/apiui/pages/BasePage.java
@@ -1,2 +1,11 @@
-package com.apiui.pages;public class BasePage {
+package com.apiui.pages;
+
+import com.apiui.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasePage {
+    public BasePage() {
+        PageFactory.initElements(Driver.get(), this);
+    }
+
 }
